6bf798f90bc3c776a2b8c8c077c8f16a258d4b5d
ShaikSumayya0553 <shaiksumayya0553@gmail.com>
2026-05-24 19:18:06 +0530

    fixed cors issue

--- backend/server.js
+++ backend/server.js
@@ -12,17 +12,22 @@ import postRoutes from "./routes/postRoutes.js";
 
 const app = express();
 
-const cors = require("cors");
+// CORS
 app.use(cors({
-  origin: "https://journal-app-orpin.vercel.app/",
+  origin: "https://journal-app-orpin.vercel.app",
   credentials: true
 }));
+
+// Middleware
 app.use(express.json());
 
+// Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/posts", postRoutes);
 
+// Server
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
